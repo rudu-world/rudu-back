6852de71218070a5671bf3c7694ff88df0bfc4ec
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-11-28 00:59:54 +0900

    [CHORE] : DOCKER TEST

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
 
-const APP_PORT = process.env.APP_PORT;
+const APP_PORT = 8081;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.get("/ping", (req, res) => res.status(200).send("pong"));
 
 app.listen(APP_PORT, () => {
   console.log(`wow ${APP_PORT}`);
